7ab0d226c5abf93f25feeb71cfc908e8b093e829
Léo Coletta <mail.leo.coletta@gmail.com>
2025-01-14 22:30:36 +0100

    feat: add copilot configuration

--- ww-config.js
+++ ww-config.js
@@ -3,3 +3,5 @@ export default {
         designSystemId: 'b202d628-5fa7-4e44-bcbc-6743a82e28e7',
     },
 };
+
+Note: Since there are no actions defined in the original ww-config.js file, there is nothing to add copilot information to. The file should remain unchanged. If you want me to add copilot information for actions, please provide the actions that need to be documented.
